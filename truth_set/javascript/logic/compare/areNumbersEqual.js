7205b14186961bfde391976f79ab770574e07393
Vivek Dixit <81433083+vivekdxt@users.noreply.github.com>
2025-11-11 17:34:34 +0530

    Replicate changes from GitLab commit 73309a28c13bbd6ff1b66a1b110bd4c0d86c818a

---
+++ truth_set/javascript/logic/compare/areNumbersEqual.js
@@ -0,0 +1,10 @@
+function areNumbersEqual(a, b) {
+    if (a = b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(areNumbersEqual(5, 5));
+console.log(areNumbersEqual(3, 4));
