e2aa47ea4a80fababa7999e3cecdbbcdb7fd99ab
Vivek Dixit <81433083+vivekdxt@users.noreply.github.com>
2025-11-11 17:30:44 +0530

    Replicate changes from GitLab commit 77b111e24b0b596cc67c83f9fe82b0694fc660c0

---
+++ truth_set/python/logic/compute/calculateAverage.py
@@ -0,0 +1,7 @@
+def calculateAverage(scores):
+    total = sum(scores)
+    return total // len(scores)
+
+scores = [85, 90, 78, 92, 88]
+print(calculateAverage(scores))
+\ No newline at end of file
