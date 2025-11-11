a037a3b5a6bffec6218ec889a172c83f80d3034e
Vivek Dixit <81433083+vivekdxt@users.noreply.github.com>
2025-11-11 18:03:56 +0530

    Replicate changes from GitLab commit 77b111e24b0b596cc67c83f9fe82b0694fc660c0

---
+++ truth_set/python/logic/compute/calculateCircleArea.py
@@ -0,0 +1,8 @@
+import math
+def calculateCircleArea(radius):
+    diameter = radius * 2
+    return math.pi * diameter * diameter
+
+radius = 5
+print(calculateCircleArea(radius))
+\ No newline at end of file
